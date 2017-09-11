4edbf5f53d502f0cd7954dff100023b6311a81f6
Edward Cree <ecree@solarflare.com>
2017-09-11 17:47:51 +0100

    Added an example program "dropper.s", simple IP-addr blacklister
    This is basically the same toy program everyone writes when getting started
     with XDP, summarised as:
        if (ip.saddr in map) {
            map[ip.saddr]++;
            return XDP_DROP;
        }
        return XDP_PASS;
    
    Also included is a header file with equates for Ethernet and IP header
     field offsets.

---
+++ dropper.s
@@ -0,0 +1,45 @@
+; dropper.s
+; simple IP-based XDP drop program
+
+.include defs.i
+.include net_hdrs.i
+
+.text
+.section prog
+	ld	r0.l, XDP_PASS ; On errors we return pass
+	ld	r2.l, [r1+XDP_MD_DATA]
+	ld	r3.l, [r1+XDP_MD_DATA_END]
+	ld	r1, r2
+	add	r1, ETHER_HDR__LEN
+	jr	ge, r3, r1, +1 ; Do we have the entire ether-hdr?
+	exit
+	ld	r4.w, [r2+ETHER_HDR_PROTO]
+	jr	z, r4, ETHERTYPE_IPV4_LE, +1 ; Is it IPv4?
+	exit
+	ld	r2, r1 ; done with the ether-hdr
+	add	r1, IP_HDR__LEN
+	jr	ge, r3, r1, +1 ; Do we have the entire IP hdr?
+	exit
+	add	r2, IP_HDR_SADDR
+	ld	r1, dropcnt
+	call	bpf_map_lookup_elem
+	jr	nz, r0, 0, drop
+	; Not in the map, pass it
+	ld	r0.l, XDP_PASS
+	exit
+drop:
+	; Increment the counter
+	ld	r1.l, [r0]
+	add	r1.l, 1
+	ld	[r0], r1.l
+	; return drop verdict
+	ld	r0.l, XDP_DROP
+	exit
+
+.section maps
+; ip.src => counter
+dropcnt: percpu_hash, 4, 4, 1024, P
+.data
+.section license
+_license:
+    asciz   "GPL"
---
+++ net_hdrs.i
@@ -0,0 +1,24 @@
+; net_hdrs.i
+; Network header offsets
+
+; Ethernet
+.equ	ETHER_HDR_DEST, 0
+.equ	ETHER_HDR_SRC, 6
+.equ	ETHER_HDR_PROTO, 12
+.equ	ETHER_HDR__LEN, 14
+
+;TODO: implement the BPF_END opcode in the assembler, so we can do this properly
+.equ	ETHERTYPE_IPV4_LE, 0x0008 ; IPv4 is 0x0800, but Ethertype is Big-Endian
+
+; IPv4
+.equ	IP_HDR_IHLVER, 0
+.equ	IP_HDR_TOS, 1
+.equ	IP_HDR_TOT_LEN, 2
+.equ	IP_HDR_ID, 4
+.equ	IP_HDR_FRAG_OFF, 6
+.equ	IP_HDR_TTL, 8
+.equ	IP_HDR_PROTOCOL, 9
+.equ	IP_HDR_CSUM, 10
+.equ	IP_HDR_SADDR, 12
+.equ	IP_HDR_DADDR, 16
+.equ	IP_HDR__LEN, 20 ; length of fixed part, excl. options
